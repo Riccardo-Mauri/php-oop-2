04d342a797e63835ca121df7c5878bbc91362d27
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-10-14 20:20:25 +0200

    aggiunti traits e un exception

--- index.php
+++ index.php
@@ -1,26 +1,47 @@
 <?php
-//Definisco la mia "Macrocategoria" 
+// Definisco dei Trait per la validazione
+trait Validatable {
+    // Metodo per validare il prezzo
+    public function validatePrice($price) {
+        if ($price < 0) {
+            throw new InvalidArgumentException("Il prezzo non può essere negativo.");
+        }
+        return true;
+    }
 
+    // Metodo per validare la quantità
+    public function validateKg($kg) {
+        if ($kg <= 0) {
+            throw new InvalidArgumentException("La quantità deve essere maggiore di zero.");
+        }
+        return true;
+    }
+}
+
+// Definisco la mia "Macrocategoria"
 class Category {
     public $name;
     public $icon;
-    //Definisco il costruttore della mia classe 
-    function __construct ($name, $icon){
+
+    function __construct($name, $icon) {
         $this->name = $name;
-        $this->icon =$icon;
+        $this->icon = $icon;
     }
 }
 
 class Products {
-    public $name; 
-    public $price; 
-    public $brand; 
+    use Validatable; // Aggiungo il trait per la validazione
+
+    public $name;
+    public $price;
+    public $brand;
     public $category;
     public $image;
 
-    //Definisco il COSTRUTTORE della mia classe
-    function __construct ($name, $price, $brand,Category $category, $image){
+    // Definisco il costruttore della mia classe
+    function __construct($name, $price, $brand, Category $category, $image) {
         $this->name = $name;
+        $this->validatePrice($price); // Validazione del prezzo
         $this->price = $price;
         $this->brand = $brand;
         $this->category = $category;
@@ -28,53 +49,60 @@ class Products {
     }
 }
 
-//Creo le altre mi classi principali che sono "Cibo - Giochi - Cucce"
-
-class Toys extends Products { 
-    //adesso quindi erediterò dal padre cioè "Products" gli attributi che mi interessano cioè tutti
+// Creo le classi principali "Cibo - Giochi - Cucce"
+class Toys extends Products {
+    // Eredita dal padre, "Products"
 }
 
-class Food extends Products { //adesso quindi erediterò dal padre cioè "Products" gli attributi che mi interessano cioè tutti E le sue proprietà uniche
-   public $size; //definisce la taglia adatta per il cibo
-   public $kg;   //definisce la quantità in kg del cibo
+class Food extends Products {
+    use Validatable; // Aggiungi il trait per la validazione
 
-   // Costruttore della classe "Food" che contiene poi le sue proprietà uniche
-    function __construct($name, $price, $brand,Category $category,$image, $size, $kg){
-        parent::__construct($name, $price, $brand, $category, $image);//richiamo il costruttore del padre 
+    public $size; // Definisce la taglia adatta per il cibo
+    public $kg;   // Definisce la quantità in kg del cibo
+
+    // Costruttore della classe "Food"
+    function __construct($name, $price, $brand, Category $category, $image, $size, $kg) {
+        parent::__construct($name, $price, $brand, $category, $image); // Richiamo il costruttore del padre
         $this->size = $size;
+        $this->validateKg($kg); // Validazione della quantità
         $this->kg = $kg;
     }
-
 }
 
-class Kennels extends Products { //adesso quindi erediterò dal padre cioè "Products" gli attributi che mi interessano cioè tutti E le sue proprietà uniche 
-   public $size; //definisce la taglia adatta della cuccia - cioè la grandezza 
+class Kennels extends Products {
+    use Validatable; // Aggiungi il trait per la validazione
 
-       // Costruttore della classe "Kennels" che contiene poi le sue proprietà uniche
-       function __construct($name, $price, $brand,Category $category,$image, $size){
-        parent::__construct($name, $price, $brand, $category, $image);//richiamo il costruttore del padre 
+    public $size; // Definisce la taglia adatta della cuccia
+
+    // Costruttore della classe "Kennels"
+    function __construct($name, $price, $brand, Category $category, $image, $size) {
+        parent::__construct($name, $price, $brand, $category, $image); // Richiamo il costruttore del padre
         $this->size = $size;
     }
-
 }
+
 $dogCategory = new Category('Cani', '🐶');
 $catCategory = new Category('Gatti', '🐱');
 
-//adesso creo i miei prodotti effettivi-variabili per i Cani
+// Creo i miei prodotti per i Cani
+try {
+    $DogToy = new Toys('Pallina per cani - Morbida', 5.99, 'FamousBrand', $dogCategory, 'https://arcaplanet.vtexassets.com/arquivos/ids/229318/trixie-cane-palla-in-gomma-naturale-con-squittio.jpg?v=637454816312200000');
 
-$DogToy = new Toys('Pallina per cani - Morbida', 5.99, 'FamousBrand', $dogCategory, 'https://arcaplanet.vtexassets.com/arquivos/ids/229318/trixie-cane-palla-in-gomma-naturale-con-squittio.jpg?v=637454816312200000');
+    $DogFood = new Food('Crocchette per cani', -39.99, 'Royal Canin', $dogCategory, 'https://www.ipelosi.it/cache/royal-canin-maxi-adult-5-kg-15-crocchette-cani_78_it-750-750.jpg', 'Grande', 5);
 
-$DogFood = new Food('Crocchette per cani', 39.99, 'Royal Canin', $dogCategory, 'https://www.ipelosi.it/cache/royal-canin-maxi-adult-5-kg-15-crocchette-cani_78_it-750-750.jpg', 'Grande', 5);
+    $DogKennel = new Kennels('Cuccia per cani morbida', 40.99, 'CareyourPet', $dogCategory, 'https://www.omlet.it/images/catalog/2022/07/05/dogs-love-resting-in-the-fido-studio-modern-dog-crate-omlet.jpg', 'Media');
 
-$DogKennel = new Kennels('Cuccia per cani morbida', 40.99, 'CareyourPet', $dogCategory,'https://www.omlet.it/images/catalog/2022/07/05/dogs-love-resting-in-the-fido-studio-modern-dog-crate-omlet.jpg', 'Media');
+    // Creo i miei prodotti per i Gatti
+    $KatToy = new Toys('Topolino di pezza', 9.99, 'FamousBrand', $catCategory, 'https://media.zooplus.com/bilder/7/400/67334_katzenspielzeug_wild_mouse_mit_sound_und_led_fg_2717_7.jpg');
 
-//adesso creo i miei prodotti effettivi-variabili per i Gatti
+    // Qui stai utilizzando 0.08, che è una quantità valida, quindi va bene
+    $KatFood = new Food('Umido per gatti', 4.99, 'Whiskas', $catCategory, 'https://cdn.onemars.net/sites/whiskas_ph_Ropir_mwh5/image/mockup_wks_pouch_ad_tuna_new-look_-80g_f_1705068714309.png', 'Piccolo', 0.08);
 
-$KatToy = new Toys('Topolino di pezza', 9.99, 'FamousBrand', $catCategory,'https://media.zooplus.com/bilder/7/400/67334_katzenspielzeug_wild_mouse_mit_sound_und_led_fg_2717_7.jpg');
+    $KatKennel = new Kennels('Cuccietta per gattini', 29.99, 'CareyourPet', $catCategory, 'https://climaconvenienza.it/cdn/shop/products/immagine-1-easycomfort-easycomfort-cuccia-per-gatti-a-2-ripiani-in-vimini-grigio-con-2-cuscini-lavabili-interno-esterno-56-37-40cm-ean-8054144132082-jpg.jpg?v=1703193474&width=1214', 'Piccolo');
 
-$KatFood = new Food('Umido per gatti', 4.99, 'Whiskas', $catCategory,  'https://cdn.onemars.net/sites/whiskas_ph_Ropir_mwh5/image/mockup_wks_pouch_ad_tuna_new-look_-80g_f_1705068714309.png', 'Piccolo', 0.08);
-
-$KatKennel = new Kennels('Cuccietta per gattini', 29.99, 'CareyourPet', $catCategory, 'https://climaconvenienza.it/cdn/shop/products/immagine-1-easycomfort-easycomfort-cuccia-per-gatti-a-2-ripiani-in-vimini-grigio-con-2-cuscini-lavabili-interno-esterno-56-37-40cm-ean-8054144132082-jpg.jpg?v=1703193474&width=1214', 'Piccolo');
+} catch (InvalidArgumentException $e) {
+    echo "Errore: " . $e->getMessage();
+}
 
 //Metto i prodotti in un Array così posso iterarli e stamparli in html
 $products = [$DogToy, $DogFood, $DogKennel, $KatToy, $KatFood, $KatKennel]
